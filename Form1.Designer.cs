08707e77ac4041c4334b3e244949acd055e3f924
acarney9 <acmustang920@gmail.com>
2018-09-16 21:01:00 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace BubbleSortExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nonSortedButton = new System.Windows.Forms.Button();
+            this.sortedButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nonSortedLabel = new System.Windows.Forms.Label();
+            this.sortedLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Non Sorted";
+            // 
+            // nonSortedButton
+            // 
+            this.nonSortedButton.Location = new System.Drawing.Point(302, 432);
+            this.nonSortedButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.nonSortedButton.Name = "nonSortedButton";
+            this.nonSortedButton.Size = new System.Drawing.Size(161, 30);
+            this.nonSortedButton.TabIndex = 2;
+            this.nonSortedButton.Text = "Non-Sorted";
+            this.nonSortedButton.UseVisualStyleBackColor = true;
+            this.nonSortedButton.Click += new System.EventHandler(this.nonSortedButton_Click);
+            // 
+            // sortedButton
+            // 
+            this.sortedButton.Location = new System.Drawing.Point(800, 432);
+            this.sortedButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sortedButton.Name = "sortedButton";
+            this.sortedButton.Size = new System.Drawing.Size(155, 30);
+            this.sortedButton.TabIndex = 5;
+            this.sortedButton.Text = "Sorted";
+            this.sortedButton.UseVisualStyleBackColor = true;
+            this.sortedButton.Click += new System.EventHandler(this.sortedButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(661, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Sorted";
+            // 
+            // nonSortedLabel
+            // 
+            this.nonSortedLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.nonSortedLabel.Location = new System.Drawing.Point(75, 29);
+            this.nonSortedLabel.Name = "nonSortedLabel";
+            this.nonSortedLabel.Size = new System.Drawing.Size(204, 1055);
+            this.nonSortedLabel.TabIndex = 6;
+            // 
+            // sortedLabel
+            // 
+            this.sortedLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.sortedLabel.Location = new System.Drawing.Point(589, 29);
+            this.sortedLabel.Name = "sortedLabel";
+            this.sortedLabel.Size = new System.Drawing.Size(204, 1027);
+            this.sortedLabel.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 1050);
+            this.Controls.Add(this.sortedLabel);
+            this.Controls.Add(this.nonSortedLabel);
+            this.Controls.Add(this.sortedButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nonSortedButton);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button nonSortedButton;
+        private System.Windows.Forms.Button sortedButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label nonSortedLabel;
+        private System.Windows.Forms.Label sortedLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BubbleSortExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //field variable
+        int[] numbers = { 136, 211, 194, 277, 231, 281, 162, 242, 348, 154, 226, 212, 368, 173, 166, 123, 241, 258, 409, 214
+        , 178, 182, 323, 264, 311, 445, 290, 196, 237, 141, 231, 434, 183, 490, 159, 389, 167, 209, 222, 362, 196, 246, 198, 472,
+        189, 175, 340, 210, 248, 260};
+
+        private void nonSortedButton_Click(object sender, EventArgs e)
+        {
+            //display non sorted array values
+
+            string nonSorted = "";
+
+            foreach (int element in numbers)
+            {
+                nonSorted = nonSorted + element + "\n";
+            }
+
+            nonSortedLabel.Text = nonSorted.ToString();
+        }
+
+        private void sortedButton_Click(object sender, EventArgs e)
+        {
+            //bubble sort this array to place the values in ascending order
+
+            int temp = 0;
+            int count = 0;
+
+            string sorted = "";
+
+            //loop
+            for (int x = 0; x < numbers.Length; x++)
+            {
+                for (int z = 0; z < numbers.Length - 1 - x; z++)
+                {
+                    if(numbers[z] > numbers[z+1])
+                    {
+                        temp = numbers[z];
+                        numbers[z] = numbers[z + 1];
+                        numbers[z + 1] = temp;
+                    }
+
+                    count++;
+                }
+            }
+
+            //display the sorted array
+            foreach(int element in numbers)
+            {
+                sorted = sorted + element + "\n";
+            }
+
+            sortedLabel.Text = sorted.ToString();
+
+            MessageBox.Show("The count value is: " + count.ToString());
+        }
+
+    }
+}
